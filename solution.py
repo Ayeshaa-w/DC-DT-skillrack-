210c07b498b53c568599af10a1141d129be93c26
Ayeshaa-w <ayeshaaazz12@gmail.com>
2026-04-03 01:51:56 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,1 +1,17 @@
-
+n=int(input())
+matrix=[]
+for i in range(n):
+  val=list(map(int,input().split()))
+  matrix.append(val)
+d1=set()
+d2=set()
+for i in range(n):
+  d1.add(matrix[i][i])
+col=n-1
+for i in range(n):
+  d2.add(matrix[i][col])
+  col-=1
+if d1==d2:
+  print("yes")
+else:
+  print("no")
