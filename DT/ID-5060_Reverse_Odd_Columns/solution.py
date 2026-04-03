9fab710bdf8a543b949c8ce2eb063e4b083c8912
Ayeshaa-w <ayeshaaazz12@gmail.com>
2026-04-03 23:37:59 +0530

    Update solution.py

--- DT/ID-5060_Reverse_Odd_Columns/solution.py
+++ DT/ID-5060_Reverse_Odd_Columns/solution.py
@@ -4,6 +4,13 @@ for i in range(n):
   val=list(map(int,input().split())
   matrix.append(val)
 for i in range(n//2):
+  for j in range(n):
+    if j%2==0:
+      matrix[i][j],matrix[n-i-1][j]=matrix[n-i-1][j],matrix[i][j]
+for i in range(n):
+  for j in range(n):
+    print(matrix[i][j],end=" ")
+  print()
            
            
            
